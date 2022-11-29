8c3e6e1b12197297a40165c2a43bb58c41f70eea
CoderNoob2021 <islam.maidul567@gmail.com>
2022-11-29 17:49:07 +0900

    v2 final

---
+++ 1. Getting Started/firstapp.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func mains() {
+	greeting := "Hello World"
+	number := 6
+	fmt.Println(greeting)
+	fmt.Println(number)
+}
---
+++ 2. Go Basics/Exercise1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstName string = "Maidul"
+	lastName := "Islam"
+
+	var currentYear int = 2022
+	birthYear := 1999
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(currentYear)
+	fmt.Println(birthYear)
+
+}
