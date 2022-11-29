5873ff0013d5e3151c8fc82cffa8603f261d7f5a
CoderNoob2021 <islam.maidul567@gmail.com>
2022-11-29 18:50:12 +0900

    v3.3

--- 3. Practice/bmi.go
+++ 3. Practice/bmi.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	//Output Information
 	fmt.Println("BMI Calculator")
+	fmt.Println("-------------")
+
+	//Prompt Input Information
+	fmt.Print("Enter your weight in kg: ")
+	fmt.Print("Enter your height in m: ")
 }
