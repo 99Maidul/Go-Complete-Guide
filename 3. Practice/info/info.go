832ab4545cbf4594214d295f7149ec35d557afc9
CoderNoob2021 <islam.maidul567@gmail.com>
2022-12-01 12:00:23 +0900

    v3 Final

---
+++ 3. Practice/info/info.go
@@ -0,0 +1,6 @@
+package info
+
+const MainTittle = "BMI Calculator"
+const MainTitleLine = "-------------"
+const WeightPrompt = "Enter your weight in (kg): "
+const HeightPrompt = "Enter your height in (m): "
