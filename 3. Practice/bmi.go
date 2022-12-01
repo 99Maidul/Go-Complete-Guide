6a2cc4791ff441bf9a4ff553ab8f6faf8e677cc5
CoderNoob2021 <islam.maidul567@gmail.com>
2022-12-01 09:50:08 +0900

    Fix Input

--- 3. Practice/bmi.go
+++ 3. Practice/bmi.go
@@ -4,8 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
 )
 
 var reader = bufio.NewReader(os.Stdin)
@@ -16,19 +14,12 @@ func main() {
 	fmt.Println("-------------")
 
 	//Prompt Input Information
-	fmt.Print("Enter your weight in kg: ")
-	WeightInput, _ := reader.ReadString('\n')
-	fmt.Print("Enter your height in m: ")
-	HeightInput, _ := reader.ReadString('\n')
+	fmt.Print("Enter your weight in (kg): ")
+	weightInput, _ := reader.ReadString('\n')
+	fmt.Print("Enter your height in (m): ")
+	heightInput, _ := reader.ReadString('\n')
 
-	//Save the input information
-	WeightInput = strings.Replace(WeightInput, "\n", "", -1)
-	HeightInput = strings.Replace(HeightInput, "\n", "", -1)
-
-	weight, _ := strconv.ParseFloat(WeightInput, 64)
-	height, _ := strconv.ParseFloat(HeightInput, 64)
-
-	fmt.Print(weight)
-	fmt.Print(height)
+	fmt.Print(weightInput)
+	fmt.Print(heightInput)
 
 }
