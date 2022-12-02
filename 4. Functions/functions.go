05af91a6810edda0ae0f98ef2cdfd8aac7fd49c7
CoderNoob2021 <islam.maidul567@gmail.com>
2022-12-02 17:37:22 +0900

    Random Number addition

--- 4. Functions/functions.go
+++ 4. Functions/functions.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func main() {
-	sum := add(1, 2)
-	fmt.Println(sum)
+	a, b := generateRandomNumber()
+	sum := add(a, b)
+	pntNum(sum)
 }
 func add(x, y int) int {
 	return x + y
+}
+
+func pntNum(num int) {
+	fmt.Println(num)
+}
+
+func generateRandomNumber() (int, int) {
+	randomNumber1 := rand.Intn(10)
+	randomNumber2 := rand.Intn(10)
+	return randomNumber1, randomNumber2
 }
