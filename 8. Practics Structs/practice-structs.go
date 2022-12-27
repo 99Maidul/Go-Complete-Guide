3b3398089c2c9a1c7748ed499b12f97ea712c4df
CoderNoob2021 <islam.maidul567@gmail.com>
2022-12-27 11:26:28 +0900

    Struct Instance in main and helper function

--- 8. Practics Structs/practice-structs.go
+++ 8. Practics Structs/practice-structs.go
@@ -1,5 +1,32 @@
 package main
 
+import "fmt"
+
+type Product struct {
+	id          string
+	title       string
+	description string
+	price       float64
+}
+
+func NewProduct(id string, t string, d string, p float64) *Product {
+	return &Product{id, t, d, p}
+}
 func main() {
+	firstProduct := Product{
+		"first-product,",
+		"A book,",
+		"A nice book,",
+		10.99,
+	}
+	secondProduct := *NewProduct(
+		"second-product,",
+		"A carpet,",
+		"A beautiful carpet,",
+		99.99,
+	)
+
+	fmt.Println(firstProduct)
+	fmt.Println(secondProduct)
 
 }
