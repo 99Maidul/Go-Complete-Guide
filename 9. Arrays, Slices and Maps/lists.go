4bd9ed7a7bd1196770cf87cab0168b80f264b080
CoderNoob2021 <islam.maidul567@gmail.com>
2023-01-01 12:16:48 +0900

    Slicing

--- 9. Arrays, Slices and Maps/lists.go
+++ 9. Arrays, Slices and Maps/lists.go
@@ -10,6 +10,12 @@ type Product struct {
 }
 
 func main() {
+	var productNames [4]string = [4]string{"A book"}
+	productNames[2] = "A carpet"
 	prices := [4]float64{10.99, 9.99, 45.99, 20.00}
 	fmt.Println(prices)
+	fmt.Println(productNames)
+
+	featuredPrices := prices[1:3]
+	fmt.Println(featuredPrices)
 }
