4299b7211c2cac3bd91f2f2d31cdf2883d503a3f
CoderNoob2021 <islam.maidul567@gmail.com>
2023-01-09 10:51:24 +0600

    maps

---
+++ 9. Arrays, Slices and Maps/lists/lists.go
@@ -0,0 +1,58 @@
+package lists
+
+import "fmt"
+
+type Product struct {
+	id          string
+	title       string
+	description string
+	price       float64
+}
+
+func main() {
+	//1 Make an array of hobby
+	hobbies := []string{"Programming", "Reading", "Writing", "Drawing", "Swimming"}
+	fmt.Println(hobbies)
+	//2 Print the first element of the array
+	//2nd & 3rd element combined
+	fmt.Println(hobbies[0])
+	fmt.Println(hobbies[1:3])
+	//3 Create a slice based on 1st element that contains 1st and 2nd element
+	mainHobbies := hobbies[:2]
+	fmt.Println(mainHobbies)
+	// 4 Reslice from 3 and change it to contation 3rd and second element from original array
+	fmt.Println(cap(mainHobbies))
+	mainHobbies = mainHobbies[1:3]
+	fmt.Println(mainHobbies)
+	//5 Create dynamic array of course goals
+	courseGoals := []string{"Learn Go", "Learn Python", "Learn Java"}
+	fmt.Println(courseGoals)
+	//6 Set 2nd element to "Learn C++" and "Learn python" to 4th element
+	courseGoals[1] = "Learn C++"
+	courseGoals = append(courseGoals, "Learn Python")
+	fmt.Println(courseGoals)
+	//7 Create Product struct and add dynamic list of structs
+	products := []Product{
+		{"first-product",
+			"A first Product",
+			"This is the first product",
+			12.99},
+		{"second-product",
+			"A second Product",
+			"This is the second product",
+			13.99},
+	}
+	fmt.Println(products)
+
+	prices := []float64{12.99, 13.99}
+	fmt.Println(prices[0:1])
+	prices[1] = 9.99
+
+	prices = append(prices, 14.99)
+	prices = prices[1:]
+	fmt.Println(prices)
+
+	discountPrices := []float64{11.99, 9.99}
+	prices = append(prices, discountPrices...)
+	fmt.Println(prices)
+}
---
+++ 9. Arrays, Slices and Maps/maps.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	websites := map[string]string{
+		"Google":   "https://www.google.com",
+		"Facebook": "https://www.facebook.com",
+	}
+	fmt.Println(websites)
+}
