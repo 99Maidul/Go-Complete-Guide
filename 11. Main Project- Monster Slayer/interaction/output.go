33ddaf6295cb6cf712b7204df3ee5f2a759b3508
CoderNoob2021 <islam.maidul567@gmail.com>
2023-01-21 10:45:03 +0600

    Game functionable

--- 11. Main Project- Monster Slayer/interaction/output.go
+++ 11. Main Project- Monster Slayer/interaction/output.go
@@ -2,6 +2,15 @@ package interaction
 
 import "fmt"
 
+type RoundData struct {
+	Action           string
+	PlayerAttackDmg  int
+	PlayerHealValue  int
+	MonsterAttackDmg int
+	PlayerHealth     int
+	MonsterHealth    int
+}
+
 func PrintGreeting() {
 	fmt.Println("MONSTER SLAYER")
 	fmt.Println("Starting a new game...")
@@ -17,6 +26,18 @@ func ShowAvailableActions(specialAtttackIsAvailable bool) {
 	if specialAtttackIsAvailable == true {
 		fmt.Println("3. Special Attack")
 	}
+}
+func PrintRoundStatistics(roundData *RoundData) {
+	if roundData.Action == "ATTACK" {
+		fmt.Printf("Player attacked the monster for %v damage.\n", roundData.PlayerAttackDmg)
+	} else if roundData.Action == "SPECIAL_ATTACK" {
+		fmt.Printf("Player performed a special attack the monster for %v damage.\n", roundData.PlayerAttackDmg)
+	} else {
+		fmt.Printf("Player healed for %v.\n", roundData.PlayerHealValue)
+	}
+	fmt.Printf("Monster attacked the player for %v damage.\n", roundData.MonsterAttackDmg)
+	fmt.Printf("Player health: %v\n", roundData.PlayerHealth)
+	fmt.Printf("Monster health: %v\n", roundData.MonsterHealth)
 }
 func DeclareWinner(winner string) {
 	fmt.Println("-------------------------")
