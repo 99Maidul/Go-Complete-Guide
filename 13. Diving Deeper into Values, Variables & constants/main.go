baadf37296b05565cf57346c546040010fcc4ca7
CoderNoob2021 <islam.maidul567@gmail.com>
2023-01-25 10:04:42 +0600

    Custom types

--- 13. Diving Deeper into Values, Variables & constants/main.go
+++ 13. Diving Deeper into Values, Variables & constants/main.go
@@ -2,17 +2,29 @@ package main
 
 import "fmt"
 
+type person struct {
+	name string
+	age  int
+}
+
+type PersonData map[string]person
+type CustomNumber int
+
+func (number CustomNumber) pow(power int) CustomNumber {
+	result := number
+	for i := 1; i < power; i++ {
+		result = result * number
+	}
+	return CustomNumber(result)
+}
+
 func main() {
+	var people PersonData = PersonData{
+		"p1": {"Maidul", 23},
+	}
+	fmt.Println(people)
 
-	hobbies := make([]string, 2, 10)
-	hobbies[0] = "Sports"
-	hobbies[1] = "Reading"
-
-	hobbies = append(hobbies, "Cooking", "Dancing")
-
-	fmt.Println(hobbies)
-
-	morehobbies := new([]string)
-	*morehobbies = append(*morehobbies, "Sports", "Reading", "Cooking", "Dancing")
-	fmt.Println(*morehobbies)
+	var dummyNumber CustomNumber = 5
+	poweredNumber := dummyNumber.pow(3)
+	fmt.Println(poweredNumber)
 }
