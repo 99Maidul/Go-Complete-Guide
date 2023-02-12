3cbe9421a63ddf5e05a899268184d13a77c19f9b
CoderNoob2021 <islam.maidul567@gmail.com>
2023-02-12 10:49:59 +0900

    New codes

--- 17. Concurrency, Goroutines & Channels/main.go
+++ 17. Concurrency, Goroutines & Channels/main.go
@@ -2,59 +2,85 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"sync"
+	"math/rand"
+	"time"
 )
 
-var lock sync.Mutex
+var source = rand.NewSource(time.Now().Unix())
+var randN = rand.New(source)
 
 func main() {
-	greet()
-	storeData("This is some dummy data!", "dummy-data.txt")
+	c := make(chan int)
 
-	channel := make(chan int)
+	go generateValue(c)
+	go generateValue(c)
 
-	go storeMoreData(5000, "5000_1.txt", channel)
-	go storeMoreData(5000, "5000_2.txt", channel)
+	x := <-c
+	y := <-c
 
-	<-channel
-	<-channel
+	sum := x + y
+
+	fmt.Println(sum)
 }
 
-func greet() {
-	fmt.Println("Hi there!")
+func generateValue(c chan int) int {
+	sleepTime := randN.Intn(3)
+	time.Sleep(time.Duration(sleepTime) * time.Second)
+
+	result := randN.Intn(10)
+	c <- result
+	return result
 }
 
-func storeData(storableText string, fileName string) {
-	lock.Lock()
-	defer lock.Unlock()
+// var lock sync.Mutex
 
-	file, err := os.OpenFile(fileName,
-		os.O_CREATE|os.O_APPEND|os.O_WRONLY,
-		0666,
-	)
+// func main() {
+// 	greet()
+// 	storeData("This is some dummy data!", "dummy-data.txt")
 
-	if err != nil {
-		fmt.Println("Creating the file failed. Exiting")
-		return
-	}
+// 	channel := make(chan int)
 
-	defer file.Close()
+// 	go storeMoreData(5000, "5000_1.txt", channel)
+// 	go storeMoreData(5000, "5000_2.txt", channel)
 
-	_, err = file.WriteString(storableText)
+// 	<-channel
+// 	<-channel
+// }
 
-	if err != nil {
-		fmt.Println("Writing to the file failed.")
-	}
-}
+// func greet() {
+// 	fmt.Println("Hi there!")
+// }
 
-func storeMoreData(lines int, fileName string, c chan int) {
-	for i := 0; i < lines; i++ {
-		text := fmt.Sprintf("line %v - Dummy Data\n", i)
-		storeData(text, fileName)
-	}
+// func storeData(storableText string, fileName string) {
+// 	lock.Lock()
+// 	defer lock.Unlock()
 
-	fmt.Printf("-- Done storing %v lines of text --\n", lines)
+// 	file, err := os.OpenFile(fileName,
+// 		os.O_CREATE|os.O_APPEND|os.O_WRONLY,
+// 		0666,
+// 	)
 
-	c <- 1
-}
+// 	if err != nil {
+// 		fmt.Println("Creating the file failed. Exiting")
+// 		return
+// 	}
+
+// 	defer file.Close()
+
+// 	_, err = file.WriteString(storableText)
+
+// 	if err != nil {
+// 		fmt.Println("Writing to the file failed.")
+// 	}
+// }
+
+// func storeMoreData(lines int, fileName string, c chan int) {
+// 	for i := 0; i < lines; i++ {
+// 		text := fmt.Sprintf("line %v - Dummy Data\n", i)
+// 		storeData(text, fileName)
+// 	}
+
+// 	fmt.Printf("-- Done storing %v lines of text --\n", lines)
+
+// 	c <- 1
+// }
